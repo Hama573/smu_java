87acd432245cd1aa50d020f1bebab18a466f190c
KHM <143370088+Hama573@users.noreply.github.com>
2023-11-09 00:48:54 +0900

    Homework5

---
+++ Homework5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Homework5 {
+    static class PersonalComputer() {
+        computer(string computer) {
+
+        }
+        monitor(string monitor) {
+            string monitorsize;
+            string color;
+            string power;
+
+            turnon();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        
+    }
+    
+}
