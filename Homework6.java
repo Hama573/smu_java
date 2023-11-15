3cf61e83c79c41c3bfd02c528434703ae28432d0
KHM <143370088+Hama573@users.noreply.github.com>
2023-11-16 04:39:09 +0900

    Homework6

---
+++ Homework6.java
@@ -0,0 +1,46 @@
+public class Homework6 {
+static class Course {
+    String code;
+    String name;
+
+    public Course(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Code: " + code + ", Name: " + name;
+    }
+}
+
+static class OnlineCourse extends Course {
+    public OnlineCourse(String code, String name) {
+        super(code, name);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Type: Online";
+    }
+}
+
+static class OfflineCourse extends Course {
+    public OfflineCourse(String code, String name) {
+        super(code, name);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Type: Offline";
+    }
+}
+
+    public static void main(String[] args) {
+        OfflineCourse course1 = new OfflineCourse("HAEA9201", "Object Oriented Programming");
+        OnlineCourse course2 = new OnlineCourse("HAFL0012", "C Programming 1");
+
+        System.out.println(course1);
+        System.out.println(course2);
+    }
+}
